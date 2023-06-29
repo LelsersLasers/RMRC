edb79a6c7906eff08a048db274f6e2b5ee73e7f8
Lord-Lelsers <69917973+Lord-Lelsers@users.noreply.github.com>
2023-06-29 11:24:53 -0700

    Comments in run_all.sh

--- CameraSetups/README.md
+++ CameraSetups/README.md
@@ -5,10 +5,10 @@ Should be able to just `./run_all.sh` from this directory.
 ## Issues
 
 - Realsense cameras sometimes don't connect
-    - Solution?
+    - Try lower power mode (15W 6 core instead of 20W)
+        - Allows more power for the cameras
+    - Other possible solution:
         - Keep rerunning script and/or rebooting until it works
-    - Try lower power mode (15W 6 core)
-    -     - Allows more power for the cameras
 - Don't press control-c while the script is running
     - It must finish to correctly close all the background realsense nodes/etc that it started
     - If this happens, just reboot to be able to run the script again
@@ -18,11 +18,17 @@ Should be able to just `./run_all.sh` from this directory.
 
 # Cameras
 
-To get camera ids/symlinks (should be stable): `ls /dev/v4l/by-id`
+- To get camera ids/symlinks (should be stable): `ls /dev/v4l/by-id`
+- If the output doesn't contain the following two entries, update the respective launch files (`launch_ir.launch` and `launch_c270.launch`)
 ```
 usb-046d_C270_HD_WEBCAM_200901010001-video-index0
 usb-GroupGets_PureThermal__fw:v1.3.0__8003001e-5119-3038-3532-373600000000-video-index0
 ```
+
+To run the proccesses individually (for either better troubleshooting or if `run_all.sh` isn't working properly):
+- Run each of the following sections in their own terminal
+    - 4 total (3 launchs and 1 for rviz)'
+- Run in the order listed (t265 & d435i first and rviz last)
 
 ## t265 & d435i
 
--- CameraSetups/run_all.sh
+++ CameraSetups/run_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Prints my output in blue so it can be seen easier and different from the outputs from the commands
 printInColor() {
     BLUE='\033[0;34m'
     NC='\033[0m' # no color
@@ -8,6 +9,7 @@ printInColor() {
 }
 
 
+# start: t265 & d435i --------------------------------------------------------#
 printInColor "STARTING! Will take ~30 seconds to launch everything."
 printInColor "(note: this script doesn't play super nice with control-c)\n"
 
@@ -17,10 +19,12 @@ sleep 1
 # also starts main realsense node
 roslaunch launch_t265_d435i.launch &
 
+# Likely could be ~3 sec, but this reduces yellow/red text (and thus unneeded stress)
 sleep 9
+# end: t265 & d435i ----------------------------------------------------------#
 
 
-
+# start: c270 ----------------------------------------------------------------#
 printInColor "Starting launching c270"
 sleep 1
 
@@ -29,41 +33,44 @@ unset GSCAM_CONFIG
 roslaunch launch_c270.launch &
 
 sleep 9
+# end: c270 ------------------------------------------------------------------#
 
 
-
+# start: ir ------------------------------------------------------------------#
 printInColor "Starting launching ir"
 sleep 1
 
-# should be fine to reuse the same env var in the same terminal...
 unset GSCAM_CONFIG
 roslaunch launch_ir.launch &
 
 sleep 9
+# end: ir --------------------------------------------------------------------#
 
 
-
-printInColor "Starting RVIZ"
+# start: rviz ----------------------------------------------------------------#
+printInColor "Starting RVIZ (try to close it by the 'X' rather than control-c)"
 rviz -d fourCameras.rviz
 printInColor "Closing RVIZ"
 
+sleep 2
+# end: rviz ------------------------------------------------------------------#
 
-sleep 5
 
-
+# start: cleanup -------------------------------------------------------------#
 printInColor "Starting closing background processes"
 printInColor "Currently running backgrounds processes:"
 jobs -r
 
 sleep 1
 
-# kill $(jobs -p) # this could fail if $(jobs -p) is empty
+# Take output of running background jobs and pass them to kill
 jobs -p | xargs kill
 
 sleep 15
 jobs
 
 printInColor "Finished cleanup"
+# end: cleanup ---------------------------------------------------------------#
 
 
 printInColor "Exiting"
