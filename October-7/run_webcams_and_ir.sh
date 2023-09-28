d859c73b4cb0f6cee716122dd6d1337fa43a742e
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-09-28 16:46:32 -0700

    Running webcam 1 through opencv instead of gstreamer

--- October-7/run_webcams_and_ir.sh
+++ October-7/run_webcams_and_ir.sh
@@ -23,14 +23,15 @@ printInColor "(note: this script doesn't play super nice with control-c)\n"
 # start: webcam 1 ------------------------------------------------------------#
 printInColor "Starting webcam 1"
 
-gst-launch-1.0 v4l2src device=/dev/v4l/by-id/usb-046d_C270_HD_WEBCAM_2D4AA0A0-video-index0 ! videoconvert ! video/x-raw,format=UYVY ! videoscale ! video/x-raw,width=320,height=240 ! videoconvert ! xvimagesink &
+# gst-launch-1.0 v4l2src device=/dev/v4l/by-id/usb-046d_C270_HD_WEBCAM_2D4AA0A0-video-index0 ! videoconvert ! video/x-raw,format=UYVY ! videoscale ! video/x-raw,width=320,height=240 ! videoconvert ! xvimagesink &
+python opencv_webcam1.py &
 
 sleep 1
 # end: webcam 1 --------------------------------------------------------------#
 
 
 # start: webcam 2 ------------------------------------------------------------#
-printInColor "Starting webcam 1"
+printInColor "Starting webcam 2"
 
 gst-launch-1.0 v4l2src device=/dev/v4l/by-id/usb-046d_C270_HD_WEBCAM_348E60A0-video-index0 ! videoconvert ! video/x-raw,format=UYVY ! videoscale ! video/x-raw,width=320,height=240 ! videoconvert ! xvimagesink &
 
