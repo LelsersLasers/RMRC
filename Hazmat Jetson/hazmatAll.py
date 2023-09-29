31e67691fea32be0615c6766cf3fbcab8bd29397
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-09-29 15:15:29 -0700

    Update hazmatAll.py

--- Hazmat Jetson/hazmatAll.py
+++ Hazmat Jetson/hazmatAll.py
@@ -194,7 +194,7 @@ def hazmat_main():
 
         cv2.rectangle(img, (x1, y1), (x2, y2), (225,0,0), 4)
 
-        corner = (x1, y1 - 5)
+        corner = (x1, y1 + 25)
 
         cv2.putText(
             img,
