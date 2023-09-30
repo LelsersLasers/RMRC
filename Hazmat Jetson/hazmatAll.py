13a6a50c1b8c192ec1643626c93ede2218803263
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2023-09-29 18:12:35 -0700

    Cleaning old code + formatting

--- Hazmat Jetson/hazmatAll.py
+++ Hazmat Jetson/hazmatAll.py
@@ -2,9 +2,8 @@
 from __future__ import print_function
 from levenshtein import *
 from cleanUp import *
-#from screenshot import *
 from opencv_webcam1 import *
-import numpy as np 
+import numpy as np
 import argparse
 import mahotas
 import pytesseract
@@ -33,7 +32,7 @@ def processScreenshot(img, val):
     img = cv2.bitwise_not(binary)
 
     if args["debug"]:
-        cv2.imshow('Inverted Binary Image', img)
+        cv2.imshow("Inverted Binary Image", img)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(img, (5, 5), 0)
@@ -45,113 +44,94 @@ def processScreenshot(img, val):
     thresh[thresh < 255] = 0
     thresh = cv2.bitwise_not(thresh)
 
-    ret,thresh = cv2.threshold(gray,50,255,0)
-    contours,hierarchy = cv2.findContours(thresh, 1, 2)
+    _, thresh = cv2.threshold(gray, 50, 255, 0)
+    contours, _ = cv2.findContours(thresh, 1, 2)
     # print("Number of contours detected:", len(contours))
 
     imageList = []
     for i, cnt in enumerate(contours):
-        x1,y1 = cnt[0][0]
-        approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
+        x1, y1 = cnt[0][0]
+        approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
         if len(approx) == 4:
             x, y, w, h = cv2.boundingRect(cnt)
-            ratio = float(w)/h
+            ratio = float(w) / h
             if w > 63:
                 count = i
                 if ratio >= 0.8 and ratio <= 1.2:
-                    # print("Length of w:",w)
-                    # print("Ratio:", ratio)
-                    img = cv2.drawContours(img, [cnt], -1, (255,0,0), 3)
-                    cv2.putText(img, 'Square', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2)
+                    img = cv2.drawContours(img, [cnt], -1, (255, 0, 0), 3)
+                    cv2.putText(
+                        img, "Square", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2
+                    )
 
                     if args["debug"]:
                         cv2.imshow("squares", img)
-                    
-                    cropped = img[y:y+h, x:x+w]
-                    cropped = cv2.bitwise_and(cropped, img[y:y+h, x:x+w])
+
+                    cropped = img[y : y + h, x : x + w]
+                    cropped = cv2.bitwise_and(cropped, img[y : y + h, x : x + w])
                     rows, cols = cropped.shape[:2]
-                    matrixCW45 = cv2.getRotationMatrix2D((cols/2, rows/2), -45, 1)
+                    matrixCW45 = cv2.getRotationMatrix2D((cols / 2, rows / 2), -45, 1)
                     cw45 = cv2.warpAffine(cropped, matrixCW45, (cols, rows))
-                    # cv2.imshow(f"45 cw {count}", cw45)
 
-                    #only need for testing purposes
-                    # matrix90 = cv2.getRotationMatrix2D((cols/2, rows/2), -90, 1)
-                    # cw90 = cv2.warpAffine(cropped, matrix90, (cols, rows))
-                    # cv2.imshow(f"90 cw {count}", cw90)
-
-                    matrixCCW45 = cv2.getRotationMatrix2D((cols/2, rows/2), 45, 1)
+                    matrixCCW45 = cv2.getRotationMatrix2D((cols / 2, rows / 2), 45, 1)
                     ccw45 = cv2.warpAffine(cropped, matrixCCW45, (cols, rows))
-                        # cv2.imshow(f"45 ccw {count}", ccw45)
 
-                        #only need for testing purposes
-                        # matrix270 = cv2.getRotationMatrix2D((cols/2, rows/2), 180, 1)
-                        # cw180 = cv2.warpAffine(cropped, matrix270, (cols, rows))
-                        # cv2.imshow(f"180 rotation {count}", cw180)
-
-                    matrixCCW135 = cv2.getRotationMatrix2D((cols/2, rows/2), 135, 1)
+                    matrixCCW135 = cv2.getRotationMatrix2D((cols / 2, rows / 2), 135, 1)
                     ccw135 = cv2.warpAffine(cropped, matrixCCW135, (cols, rows))
-                        # cv2.imshow(f"135 ccw {count}", ccw135)
 
-                    matrixCW135 = cv2.getRotationMatrix2D((cols/2, rows/2), -135, 1)
+                    matrixCW135 = cv2.getRotationMatrix2D((cols / 2, rows / 2), -135, 1)
                     cw135 = cv2.warpAffine(cropped, matrixCW135, (cols, rows))
-                        # cv2.imshow(f"135 cw {count}", cw135)
+
                     imageList.append((cw45, count, cnt))
                     imageList.append((ccw45, count, cnt))
                     imageList.append((ccw135, count, cnt))
-                    imageList.append((cw135, count, cnt))  
+                    imageList.append((cw135, count, cnt))
                     imageList.append((cropped, count, cnt))
-            
+
     myDict = {}
     for i, (image, count, cnt) in enumerate(imageList):
         width, height, _ = image.shape
-        
+
         # TODO: how do these calculations work???
         x1 = int(0)
-        y1 = int(height/2 - (height*0.15))
+        y1 = int(height / 2 - (height * 0.15))
         x2 = int(width)
-        y2 = int(height/2 + (height*0.25))
-
+        y2 = int(height / 2 + (height * 0.25))
 
         onlyText = image[y1:y2, x1:x2]
         if args["debug"]:
-            cv2.rectangle(image, (x1, y1), (x2, y2), (225,0,0), 2)
+            cv2.rectangle(image, (x1, y1), (x2, y2), (225, 0, 0), 2)
             cv2.imshow(f"image {i}", onlyText)
         text = pytesseract.pytesseract.image_to_string(onlyText, config="--psm 6")
         text = removeSpecialCharacter(text)
-        # if text == "":
-        #     text = pytesseract.pytesseract.image_to_string(onlyText, config="--psm 6")
         if text != "":
-            # print(f"Image to string for image {i}: {text}")
             myDict.update({text: (cnt)})
 
-    # print(myDict)
-    words = ["explosive", "blasting agent", "non flammable gas", "inhalation hazard", "infectious substance", "flammable liquid", 
-    "spontaneously combustible", "dangerous when wet", "oxidizer", "organic peroxide", "poison", "corrosive", "flammable gas"]
-    # correct = []
+    words = [
+        "explosive",
+        "blasting agent",
+        "non flammable gas",
+        "inhalation hazard",
+        "infectious substance",
+        "flammable liquid",
+        "spontaneously combustible",
+        "dangerous when wet",
+        "oxidizer",
+        "organic peroxide",
+        "poison",
+        "corrosive",
+        "flammable gas",
+    ]
+
     correct_tups = []
-    for key in myDict:  
+    for key in myDict:
         word = key
         closest, distance = checkList(word, words)
-        ratio = distance/len(closest)
+        ratio = distance / len(closest)
         if ratio <= 0.55:
-            # correct.append(closest)
-
-            cnt  = myDict[key]
-            
+            cnt = myDict[key]
             tup = (closest, cnt)
-
-            # correct.append(closest)
             correct_tups.append(tup)
 
-            # print(f"for image #{myDict[key]}")
-            # print(f"the starting word is {key}", end="")
-            # print()
-            # print(f"the closest value is {closest}")
-            # # print(f"the distance is {distance}")
-            # # print(f"ratio: {ratio}")
-            # print()
-    # correct = list(set(correct))
-    # return correct
     correct_tups = remove_dups(correct_tups, lambda x: x[0])
     return correct_tups
 
@@ -163,21 +143,18 @@ def remove_dups(list, comp):
             new_list.append(item)
     return new_list
 
+
 def findMax(list):
     return max(list, key=len)
 
+
 def hazmat_main():
-    # Capture the screenshot
-    # expected = int(input("Enter number of hazmat labels you see: "))
     img = screenshot()
 
-    received = []
-    threshVals = [90, 100, 110, 120, 130, 140, 150, 160, 170] 
-    count = 0
+    threshVals = [90, 100, 110, 120, 130, 140, 150, 160, 170]
     found_this_frame = []
     for threshVal in threshVals:
         received_tups = processScreenshot(img, threshVal)
-        # print(f"for threshVal of {threshVal}: {received}")
 
         for r in received_tups:
             found_this_frame.append(r)
@@ -194,12 +171,12 @@ def hazmat_main():
     for found in found_this_frame:
         text, cnt = found
 
-        img = cv2.drawContours(img, [cnt], -1, (255,0,0), 3)
+        img = cv2.drawContours(img, [cnt], -1, (255, 0, 0), 3)
         x, y, w, h = cv2.boundingRect(cnt)
 
         print(text, "\t")
 
-        cv2.rectangle(img, (x, y), (x+w, y+h), (225,0,0), 4)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (225, 0, 0), 4)
 
         corner = (x + 5, y + 15)
 
@@ -213,15 +190,14 @@ def hazmat_main():
             thickness,
             lineType,
         )
-        
+
     print("")
 
     cv2.imshow("hazmat image", img)
 
-
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    
+
 
 if __name__ == "__main__":
     hazmat_main()
