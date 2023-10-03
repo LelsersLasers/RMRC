990242b9a36e24dd6f44c7fea9d59733d846cba6
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-10-03 16:06:42 -0700

    Update run_webcams_and_ir.sh

--- Current/CamerasHazmatQR/run_webcams_and_ir.sh
+++ Current/CamerasHazmatQR/run_webcams_and_ir.sh
@@ -24,8 +24,8 @@ printInColor "(note: this script doesn't play super nice with control-c)\n"
 printInColor "Starting webcam 1"
 
 # gst-launch-1.0 v4l2src device=/dev/v4l/by-id/usb-046d_C270_HD_WEBCAM_2D4AA0A0-video-index0 ! videoconvert ! video/x-raw,format=UYVY ! videoscale ! video/x-raw,width=320,height=240 ! videoconvert ! xvimagesink &
-# python3 webcam.py &
-python3 webcam_live_hazmat_qr_multiprocessing.py &
+python3 webcam.py &
+# python3 webcam_live_hazmat_qr_multiprocessing.py &
 
 sleep 1
 # end: webcam 1 --------------------------------------------------------------#
