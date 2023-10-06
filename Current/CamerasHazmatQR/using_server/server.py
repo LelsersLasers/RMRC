4250ea071e3e8d13bb316c3eecfbb3b37815d00f
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2023-10-05 19:24:46 -0700

    logging.WARNING instead of ERROR

--- Current/CamerasHazmatQR/using_server/server.py
+++ Current/CamerasHazmatQR/using_server/server.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
     
     if not args["debug"]:
         log = logging.getLogger('werkzeug')
-        log.setLevel(logging.ERROR)
+        log.setLevel(logging.WARNING)
 
     app.run(debug=args["debug"], port=5000, host='0.0.0.0')
 
