697b9b73a3b5f69f77750729f7f7a57141f853bf
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-10-13 22:41:50 -0700

    Reverting attempts to make squares not draw on top of each other

--- Current/CamerasHazmatQR/util.py
+++ Current/CamerasHazmatQR/util.py
@@ -70,5 +70,5 @@ def removeSpecialCharacter(s):
             t += i
     return t
 
-def rects_overlap(r1, r2):
-    return not (r1[0] + r1[2] <= r2[0] or r2[0] + r2[2] <= r1[0] or r1[1] + r1[3] <= r2[1] or r2[1] + r2[3] <= r1[1])
+# def rects_overlap(r1, r2):
+#     return not (r1[0] + r1[2] <= r2[0] or r2[0] + r2[2] <= r1[0] or r1[1] + r1[3] <= r2[1] or r2[1] + r2[3] <= r1[1])
