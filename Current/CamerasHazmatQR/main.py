e95e370b7bd6cd8b60f2c37e7e5bc1a3377281fe
Lord-Lelsers <69917973+Lord-Lelsers@users.noreply.github.com>
2023-11-07 16:47:52 -0800

    Tryign to manually change the FPS

--- Current/CamerasHazmatQR/main.py
+++ Current/CamerasHazmatQR/main.py
@@ -1,7 +1,7 @@
 # Runs 2 webcams, IR, QR detection, and hazmat detection.
 
 CAP_ARGS = {
-    "webcam1": "v4l2src device=/dev/v4l/by-id/usb-046d_C270_HD_WEBCAM_2D4AA0A0-video-index0 ! videoconvert ! video/x-raw,format=UYVY ! videoscale ! video/x-raw,width=320,height=240 ! videoconvert ! appsink",
+    "webcam1": "v4l2src device=/dev/v4l/by-id/usb-046d_C270_HD_WEBCAM_2D4AA0A0-video-index0 ! videoconvert ! video/x-raw,format=UYVY ! videoscale ! video/x-raw,width=320,height=240 ! videorate ! framerate=5/1 ! videoconvert ! appsink",
     "webcam2": "v4l2src device=/dev/v4l/by-id/usb-046d_C270_HD_WEBCAM_348E60A0-video-index0 ! videoconvert ! video/x-raw,format=UYVY ! videoscale ! video/x-raw,width=320,height=240 ! videoconvert ! appsink",
     "ir": "v4l2src device=/dev/v4l/by-id/usb-GroupGets_PureThermal__fw:v1.3.0__8003000b-5113-3238-3233-393800000000-video-index0 ! videoconvert ! appsink",
 }
