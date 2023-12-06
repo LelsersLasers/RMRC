2839c7fc53ffd91327b8ee9ed6ab9a43831d8971
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-12-05 16:33:00 -0800

    Removing old files

--- Current/Motors/i2c_test.py
+++
@@ -1,8 +0,0 @@
-import smbus, time
-bus = smbus.SMBus(1)
-
-while True: 
-    servo = int(input("Servo: "))
-    pos = int(input("Position: "))
-    bus.write_byte(37, servo)
-    bus.write_byte(37, pos)
