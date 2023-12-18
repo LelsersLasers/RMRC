26dc7b5f3fa041bd1fb64a2d39e39ee5942e71e7
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-12-18 12:26:36 -0800

    Operating mode command for motors
    Seems to work without it set, but just to be explicit about it

--- Main/motors.py
+++ Main/motors.py
@@ -1,6 +1,7 @@
 import dynamixel_sdk
 
 
+# ---------------------------------------------------------------------------- #
 DEVICE_NAME = "/dev/ttyUSB0"
 PROTOCOL_VERSION = 2.0
 
@@ -11,21 +12,33 @@ ADDR_BAUDRATE, BAUDRATE, BAUDRATE_VALUE = 8, 4_500_000, 7
 ADDR_RETURN_DELAY_TIME, RETURN_DELAY_TIME_VALUE = 9, 0
 # https://emanual.robotis.com/docs/en/dxl/x/xm430-w350/#return-delay-time9
 
+ADDR_OPERATING_MODE, OPERATING_MODE_VALUE = 11, 1 # 1 = velocity control mode
+# https://emanual.robotis.com/docs/en/dxl/x/xm430-w350/#operating-mode11
+
 # MAX_VELOCITY = 330
 ADDR_VELOCITY_LIMIT, VELOCITY_LIMIT_VALUE, VELOCITY_LIMIT_UNITS = 44, 1023, 0.229 # rev/min
 # https://emanual.robotis.com/docs/en/dxl/x/xm430-w350/#velocity-limit44
 
 ADDR_TORQUE_ENABLE = 64 # 1 = enable, 0 = disable
+# https://emanual.robotis.com/docs/en/dxl/x/xm430-w350/#torque-enable64
+
 ADDR_GOAL_VELOCITY = 104
+# https://emanual.robotis.com/docs/en/dxl/x/xm430-w350/#goal-velocity104
 
 ADDR_PROFILE_ACCELERATION, PROFILE_ACCELERATION_UNITS = 108, 214.577 # rev/min^2
 TIME_TO_MAX_VELOCITY = 1 / 60 # min
+# (rev/min) / (min) = rev/min^2
 PROFILE_ACCELERATION_VALUE = int((VELOCITY_LIMIT_VALUE * VELOCITY_LIMIT_UNITS) / TIME_TO_MAX_VELOCITY / PROFILE_ACCELERATION_UNITS)
 # https://emanual.robotis.com/docs/en/dxl/x/xm430-w350/#profile-acceleration108
 
 ADDR_PRESENT_VELOCITY = 128
+# https://emanual.robotis.com/docs/en/dxl/x/xm430-w350/#present-velocity128
 ADDR_ERROR_CODE = 70
+# https://emanual.robotis.com/docs/en/dxl/x/xm430-w350/#error-status70
+# ---------------------------------------------------------------------------- #
 
+
+# ---------------------------------------------------------------------------- #
 DYNAMIXEL_IDS = { # DYNAMIXEL_IDS[side] = [id1, id2]
 	"left": [1, 3],
 	"right": [2, 4],
@@ -34,7 +47,10 @@ ORIENTATIONS = { # ORIENTATIONS[side] = direction multiplier
 	"left": 1,
 	"right": -1,
 }
+# ---------------------------------------------------------------------------- #
 
+
+# ---------------------------------------------------------------------------- #
 class DynamixelController:
 	def __init__(self, tx_rx):
 		self.port_handler = dynamixel_sdk.PortHandler(DEVICE_NAME)
@@ -71,6 +87,7 @@ class DynamixelController:
 				for addr, value in [
 					(ADDR_BAUDRATE, BAUDRATE_VALUE),
 					(ADDR_RETURN_DELAY_TIME, RETURN_DELAY_TIME_VALUE),
+					(ADDR_OPERATING_MODE, OPERATING_MODE_VALUE),
 					(ADDR_VELOCITY_LIMIT, VELOCITY_LIMIT_VALUE),
 					(ADDR_TORQUE_ENABLE, 1),
 					(ADDR_PROFILE_ACCELERATION, PROFILE_ACCELERATION_VALUE),
@@ -150,6 +167,7 @@ class DynamixelController:
 				if error_code > 0:
 					print(f"error_code {id} {error_code}")
 					self.reset_motors()
+# ---------------------------------------------------------------------------- #
 
 
 """
