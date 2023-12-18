8899e29c80d0b4698c004adbbfee272b9d590c77
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-12-18 09:15:13 -0800

    Moving old code

--- Current/Motors/Client/motor.sh
+++
@@ -1,4 +0,0 @@
-!#/bin/bash
-cd 2022_Robotics
-python3.5 pyPS4.py &
-python3.5 send_motor_speed_v1.py
--- Current/Motors/Client/motor2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd 2022_Robotics
-python3.11 pyWASD3.py &
-python3.11 send_motor_speed_v1.py
