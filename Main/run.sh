a36ab0cafd393e18310f0ecdc88a46c82417b743
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-02-06 15:47:56 -0800

    Adding latency_timer to run.sh
    it gets reset when you replug in the motors so have to set it every time
    bash -c: https://www.adamsdesk.com/posts/sudo-echo-permission-denied/

--- Main/run.sh
+++ Main/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 chmod 777 /dev/ttyUSB0
+bash -c "echo 1 > /sys/bus/usb-serial/devices/ttyUSB0/latency_timer"
 
 tegrastats --interval 20 --logfile tegrastats.log --start # run in background
 sleep 1
