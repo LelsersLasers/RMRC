da780e2b66491319aaf72dc3536513fe3f1a9c4d
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-05-20 12:17:29 -0700

    Fixing typo bug

--- main/laptop/consts.py
+++ main/laptop/consts.py
@@ -1,8 +1,8 @@
 BASE_TEST_URL = "http://127.0.0.1:5001"
 BASE_URL      = "http://192.168.1.6:5001"
 
-ARM_TEST_URL = BASE_URL + "/arm/" # + j1 + "/" + j2 + "/" + j3 + "/" + fps + "/" + time
-ARM_URL      = BASE_URL + "/arm/" # + j1 + "/" + j2 + "/" + j3 + "/" + fps + "/" + time
+ARM_TEST_URL = BASE_TEST_URL + "/arm/" # + j1 + "/" + j2 + "/" + j3 + "/" + fps + "/" + time
+ARM_URL      = BASE_URL      + "/arm/" # + j1 + "/" + j2 + "/" + j3 + "/" + fps + "/" + time
 
 READER_TEST_FPS = 10
 READER_TEST_DRIFT_RESET = 80
