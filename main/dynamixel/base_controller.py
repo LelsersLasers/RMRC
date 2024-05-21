f0e876a187398ff842f0b5dcc74d49d45d667965
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-05-21 16:20:59 -0700

    Longer wait

--- main/dynamixel/base_controller.py
+++ main/dynamixel/base_controller.py
@@ -20,7 +20,7 @@ ADDR_GOAL_POS         = 116
 ADDR_PRESENT_VELOCITY = 128
 ADDR_PRESENT_POS      = 132
 
-SHORT_WAIT = 0.1
+SHORT_WAIT = 0.5
 # ---------------------------------------------------------------------------- #
 
 
