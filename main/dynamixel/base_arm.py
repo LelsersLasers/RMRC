4a7a2158c3b639f00167323d790221958cd03f27
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-05-22 10:27:38 -0700

    J1 also uses relative rest pos instead of accidentally spinning full cycles
    So

--- main/dynamixel/base_arm.py
+++ main/dynamixel/base_arm.py
@@ -58,18 +58,17 @@ class BaseArm(dynamixel.base_controller.BaseController):
             joint_id = self.joint_ids[joint]
 
             base_rest_pos = dynamixel.arm_consts.ARM_REST_POSES[joint]
+            low_rest_pos = 4096 * (starting_poses["j2"][0] // 4096) + base_rest_pos
+            high_rest_pos = low_rest_pos + 4096
 
             if joint == "j2":
-                low_rest_pos = 4096 * (starting_poses["j2"][0] // 4096) + base_rest_pos
-                high_rest_pos = low_rest_pos + 4096
-
                 # j2 can not cross 2048 which is straight down as it moves to rest position
                 if 2048 < starting_poses["j2"][1] < base_rest_pos:
                     rest_pos = high_rest_pos
                 else:
                     rest_pos = low_rest_pos
             else:
-                rest_pos = base_rest_pos
+                rest_pos = low_rest_pos
 
             dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
                 self.port_handler,
