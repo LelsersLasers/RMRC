22fe820400c94f90673d0965cd1f1f5ab4e34a92
Victor Joulin-Batejat <victorjb2015@gmail.com>
2024-05-28 14:59:08 -0700

    added bash + commands.txt file to run gmapping in single step

---
+++ gmapping/slam_gmapping.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "granting 777 permission to USB0 port, where Lidar should be connected..."
+
+sudo chmod 777 /dev/ttyUSB0
+
+echo "sourcing catkin_ws/deve/setup.bash so all commands are recognized..."
+
+source /home/student/Desktop/catkin_ws/devel/setup.bash
+
+echo "sourcing .imu_venv to activate with all imu dependencies..."
+
+source .imu_venv/bin/activate
+
+echo "starting all slam processes in parallel with 2-sec delay"
+
+cat slam_commands.txt | parallel --delay 2
+
+killall -9 rosmaster
+
+echo "all processes should have ended."
+
+
