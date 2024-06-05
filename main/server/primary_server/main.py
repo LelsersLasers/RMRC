f80904ea06437e2888460945c3301e6da4ec5d02
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2024-06-05 09:30:49 -0700

    Naming consistency change

--- main/server/primary_server/main.py
+++ main/server/primary_server/main.py
@@ -22,7 +22,7 @@ def process(primary_server_dq, primary_server_motor_dq):
     fps_controller = shared_util.FPSController()
 
     primary_server_ds = shared_util.DoubleState(server.primary_server.consts.STATE_FROM_MASTER, server.primary_server.consts.STATE_FROM_SELF)
-    server_motor_ds = shared_util.DoubleState(motors.consts.STATE_FROM_SERVER, pickle.dumps(motors.consts.STATE_FROM_SELF))
+    primary_server_motor_ds = shared_util.DoubleState(motors.consts.STATE_FROM_SERVER, pickle.dumps(motors.consts.STATE_FROM_SELF))
 
     @app.route("/")
     def index():
@@ -38,8 +38,8 @@ def process(primary_server_dq, primary_server_motor_dq):
     @app.route("/time_offset/<offset>", methods=["GET"])
     def time_offset(offset):
         offset = float(offset) / 1000
-        server_motor_ds.s1["time_offset"] = offset
-        server_motor_ds.put_s1(primary_server_motor_dq)
+        primary_server_motor_ds.s1["time_offset"] = offset
+        primary_server_motor_ds.put_s1(primary_server_motor_dq)
         return server.util.create_response(offset)
 
     @app.route("/invert/<value>", methods=["GET"])
@@ -58,12 +58,12 @@ def process(primary_server_dq, primary_server_motor_dq):
     def config(type, key, value):
         if type == "motor":
             if key == "velocity_limit":
-                server_motor_ds.s1["velocity_limit"]["value"] = int(value)
-                server_motor_ds.s1["velocity_limit"]["count"] += 1
-                server_motor_ds.put_s1(primary_server_motor_dq)
+                primary_server_motor_ds.s1["velocity_limit"]["value"] = int(value)
+                primary_server_motor_ds.s1["velocity_limit"]["count"] += 1
+                primary_server_motor_ds.put_s1(primary_server_motor_dq)
             else:
-                server_motor_ds.s1[key] = int(value)
-                server_motor_ds.put_s1(primary_server_motor_dq)
+                primary_server_motor_ds.s1[key] = int(value)
+                primary_server_motor_ds.put_s1(primary_server_motor_dq)
         else:
             try:
                 primary_server_ds.s2[key] = int(value)
@@ -75,17 +75,17 @@ def process(primary_server_dq, primary_server_motor_dq):
     
     @app.route("/write_every_frame/<value>", methods=["GET"])
     def write_every_frame(value):
-        server_motor_ds.s1["write_every_frame"] = value == "true"
-        server_motor_ds.put_s1(primary_server_motor_dq)
+        primary_server_motor_ds.s1["write_every_frame"] = value == "true"
+        primary_server_motor_ds.put_s1(primary_server_motor_dq)
         return server.util.create_response(value)
     
     @app.route("/power/<left>/<right>/", methods=["GET"])
     def power(left, right):
         # Has percent power built into values
-        server_motor_ds.s1["left"]  = float(left)
-        server_motor_ds.s1["right"] = float(right)
-        server_motor_ds.s1["count"] += 1
-        server_motor_ds.put_s1(primary_server_motor_dq)
+        primary_server_motor_ds.s1["left"]  = float(left)
+        primary_server_motor_ds.s1["right"] = float(right)
+        primary_server_motor_ds.s1["count"] += 1
+        primary_server_motor_ds.put_s1(primary_server_motor_dq)
 
         return server.util.create_response({
             "left": left,
@@ -95,8 +95,8 @@ def process(primary_server_dq, primary_server_motor_dq):
     
     @app.route("/arm_active/<value>", methods=["GET"])
     def arm_active(value):
-        server_motor_ds.s1["arm_active"] = value == "true"
-        server_motor_ds.put_s1(primary_server_motor_dq)
+        primary_server_motor_ds.s1["arm_active"] = value == "true"
+        primary_server_motor_ds.put_s1(primary_server_motor_dq)
         return server.util.create_response(value)
 
     @app.route("/run/<detection>/<state>/", methods=["GET"])
@@ -119,13 +119,13 @@ def process(primary_server_dq, primary_server_motor_dq):
         start = time.time()
 
         primary_server_ds.update_s1(primary_server_dq)
-        server_motor_ds.update_s2(primary_server_motor_dq)
+        primary_server_motor_ds.update_s2(primary_server_motor_dq)
         
         # Note: a pickled dict is directly put into q2 instead of the original dict
-        unpickled_server_motor_ds_s2 = pickle.loads(server_motor_ds.s2)
+        unpickled_server_motor_ds_s2 = pickle.loads(primary_server_motor_ds.s2)
 
-        server_motor_ds.s1["last_get"] = time.time()
-        server_motor_ds.put_s1(primary_server_motor_dq)
+        primary_server_motor_ds.s1["last_get"] = time.time()
+        primary_server_motor_ds.put_s1(primary_server_motor_dq)
 
         # combine main info with motor info
         primary_server_ds.s1.update(unpickled_server_motor_ds_s2)
