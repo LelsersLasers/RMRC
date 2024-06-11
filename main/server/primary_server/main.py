c825ed7a517e2cc13d6b782142458d2b7317d89c
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-06-11 09:47:37 -0700

    Servers are multi threaded but have threading locks so shouldn't break things?

--- main/server/primary_server/main.py
+++ main/server/primary_server/main.py
@@ -3,6 +3,8 @@ import time
 import flask
 import logging
 
+import threading
+
 import shared_util
 
 import server.util
@@ -24,6 +26,10 @@ def process(primary_server_dq, primary_server_motor_dq):
     primary_server_ds = shared_util.DoubleState(server.primary_server.consts.STATE_FROM_MASTER, server.primary_server.consts.STATE_FROM_SELF)
     primary_server_motor_ds = shared_util.DoubleState(motors.consts.STATE_FROM_SERVER, pickle.dumps(motors.consts.STATE_FROM_SELF))
 
+    primary_server_s2_lock = threading.Lock()
+    primary_server_motor_s1_lock = threading.Lock()
+
+
     @app.route("/")
     def index():
         return flask.render_template("index.html")
@@ -37,50 +43,56 @@ def process(primary_server_dq, primary_server_motor_dq):
     
     @app.route("/time_offset/<offset>", methods=["GET"])
     def time_offset(offset):
-        offset = float(offset) / 1000
-        primary_server_motor_ds.s1["time_offset"] = offset
-        primary_server_motor_ds.put_s1(primary_server_motor_dq)
+        with primary_server_motor_s1_lock:
+            primary_server_motor_ds.s1["time_offset"] = float(offset) / 1000
+            primary_server_motor_ds.put_s1(primary_server_motor_dq)
         return server.util.create_response(offset)
 
     @app.route("/invert/<value>", methods=["GET"])
     def invert(value):
-        primary_server_motor_ds.s1["invert"] = value == "true"
-        primary_server_motor_ds.put_s1(primary_server_motor_dq)
+        with primary_server_motor_s1_lock:
+            primary_server_motor_ds.s1["invert"] = value == "true"
+            primary_server_motor_ds.put_s1(primary_server_motor_dq)
 
-        primary_server_ds.s2["invert"] = value == "true"
-        primary_server_ds.put_s2(primary_server_dq)
+        with primary_server_s2_lock:
+            primary_server_ds.s2["invert"] = value == "true"
+            primary_server_ds.put_s2(primary_server_dq)
         
         return server.util.create_response(value)
     
     @app.route("/show_detections/<value>", methods=["GET"])
     def show_detections(value):
-        primary_server_ds.s2["show_detections"] = value == "true"
-        primary_server_ds.put_s2(primary_server_dq)
+        with primary_server_s2_lock:
+            primary_server_ds.s2["show_detections"] = value == "true"
+            primary_server_ds.put_s2(primary_server_dq)
         return server.util.create_response(value)
 
     @app.route("/config/<type>/<key>/<value>", methods=["GET"])
     def config(type, key, value):
         if type == "motor":
-            if key == "velocity_limit":
-                primary_server_motor_ds.s1["velocity_limit"]["value"] = int(value)
-                primary_server_motor_ds.s1["velocity_limit"]["count"] += 1
-                primary_server_motor_ds.put_s1(primary_server_motor_dq)
-            else:
-                primary_server_motor_ds.s1[key] = int(value)
-                primary_server_motor_ds.put_s1(primary_server_motor_dq)
+            with primary_server_motor_s1_lock:
+                if key == "velocity_limit":
+                    primary_server_motor_ds.s1["velocity_limit"]["value"] = int(value)
+                    primary_server_motor_ds.s1["velocity_limit"]["count"] += 1
+                    primary_server_motor_ds.put_s1(primary_server_motor_dq)
+                else:
+                    primary_server_motor_ds.s1[key] = int(value)
+                    primary_server_motor_ds.put_s1(primary_server_motor_dq)
         else:
-            try:
-                primary_server_ds.s2[key] = int(value)
-            except ValueError:
-                primary_server_ds.s2[key] = float(value)
-            primary_server_ds.put_s2(primary_server_dq)
+            with primary_server_s2_lock:
+                try:
+                    primary_server_ds.s2[key] = int(value)
+                except ValueError:
+                    primary_server_ds.s2[key] = float(value)
+                primary_server_ds.put_s2(primary_server_dq)
 
         return server.util.create_response(value)
     
     @app.route("/write_every_frame/<value>", methods=["GET"])
     def write_every_frame(value):
-        primary_server_motor_ds.s1["write_every_frame"] = value == "true"
-        primary_server_motor_ds.put_s1(primary_server_motor_dq)
+        with primary_server_motor_s1_lock:
+            primary_server_motor_ds.s1["write_every_frame"] = value == "true"
+            primary_server_motor_ds.put_s1(primary_server_motor_dq)
         return server.util.create_response(value)
     
     @app.route("/arm_active/<value>", methods=["GET"])
@@ -91,8 +103,9 @@ def process(primary_server_dq, primary_server_motor_dq):
 
     @app.route("/run/<detection>/<state>/", methods=["GET"])
     def run(detection, state):
-        primary_server_ds.s2["run"][detection] = state == "true"
-        primary_server_ds.put_s2(primary_server_dq)
+        with primary_server_s2_lock:
+            primary_server_ds.s2["run"][detection] = state == "true"
+            primary_server_ds.put_s2(primary_server_dq)
         return server.util.create_response({
             "detection": detection,
             "state": state,
@@ -100,8 +113,9 @@ def process(primary_server_dq, primary_server_motor_dq):
 
     @app.route("/clear/<detection>/", methods=["GET"])
     def clear(detection):
-        primary_server_ds.s2["clear"][detection] += 1
-        primary_server_ds.put_s2(primary_server_dq)
+        with primary_server_s2_lock:
+            primary_server_ds.s2["clear"][detection] += 1
+            primary_server_ds.put_s2(primary_server_dq)
         return server.util.create_response(detection)
     
     @app.route("/get", methods=["GET"])
@@ -114,8 +128,9 @@ def process(primary_server_dq, primary_server_motor_dq):
         # Note: a pickled dict is directly put into q2 instead of the original dict
         unpickled_server_motor_ds_s2 = pickle.loads(primary_server_motor_ds.s2)
 
-        primary_server_motor_ds.s1["last_get"] = time.time()
-        primary_server_motor_ds.put_s1(primary_server_motor_dq)
+        with primary_server_motor_s1_lock:
+            primary_server_motor_ds.s1["last_get"] = time.time()
+            primary_server_motor_ds.put_s1(primary_server_motor_dq)
 
         # combine main info with motor info
         primary_server_ds.s1.update(unpickled_server_motor_ds_s2)
@@ -126,7 +141,7 @@ def process(primary_server_dq, primary_server_motor_dq):
         primary_server_ds.s1["fpses"][-1] = fps_controller.fps()
 
         end = time.time()
-        # print("GET:", (end - start) * 1000, "ms")
+        print("GET:", (end - start) * 1000, "ms")
 
 
         frames_dict = primary_server_ds.s1["frames"].copy()
@@ -140,4 +155,4 @@ def process(primary_server_dq, primary_server_motor_dq):
         return server.util.create_response(response_dict)
 
 
-    app.run(debug=False, port=server.primary_server.consts.PORT, host="0.0.0.0", threaded=False, processes=1)
+    app.run(debug=False, port=server.primary_server.consts.PORT, host="0.0.0.0", threaded=True, processes=1)
