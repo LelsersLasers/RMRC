50e0ab8148a82508eae3f8c134905f6b2370676d
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-06-17 11:38:10 -0700

    Update run_jetson.sh

--- main/run_jetson.sh
+++ main/run_jetson.sh
@@ -3,8 +3,8 @@
 # jetson_clocks without any options: set CPU, GPU, and EMC clocks to maximum
 sudo jetson_clocks
 
-chmod 777 /dev/ttyUSB0
-bash -c "echo 1 > /sys/bus/usb-serial/devices/ttyUSB0/latency_timer"
+sudo chmod 777 /dev/ttyUSB0
+sudo bash -c "echo 1 > /sys/bus/usb-serial/devices/ttyUSB0/latency_timer"
 
 tegrastats --interval 20 --logfile tegrastats.log --start # run in background
 sleep 1
