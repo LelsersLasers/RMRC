aa27a1fa60c5197240bf4807cda725073003476e
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-06-17 11:38:02 -0700

    I think improving run scripts

--- main/run_jetson.sh
+++ main/run_jetson.sh
@@ -9,6 +9,9 @@ bash -c "echo 1 > /sys/bus/usb-serial/devices/ttyUSB0/latency_timer"
 tegrastats --interval 20 --logfile tegrastats.log --start # run in background
 sleep 1
 
+export LD_PRELOAD=/usr/local/lib/python3.8/dist-packages/torch.libs/libgomp-4dbbc2f2.so.1.0.0
+echo "LD_PRELOAD=$LD_PRELOAD\n"
+
 python3 main_jetson.py "$@"
 sleep 1
 
--- main/run_laptop.sh
+++ main/run_laptop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod 777 /dev/ttyUSB0
-bash -c "echo 1 > /sys/bus/usb-serial/devices/ttyUSB0/latency_timer"
+sudo chmod 777 /dev/ttyUSB0
+sudo bash -c "echo 1 > /sys/bus/usb-serial/devices/ttyUSB0/latency_timer"
 
 python3 main_laptop.py "$@"
