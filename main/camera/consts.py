b702f6c67edacfc313695413727eceab4631b8a2
Charlie Fredberg <charliefredberg@outlook.com>
2024-07-13 10:20:23 -0700

    working on pi camera.  the command is putting the frames in gpu ram instead of cpu ram so dr j is working on using cuda to put them in cpu ram

--- main/camera/consts.py
+++ main/camera/consts.py
@@ -5,8 +5,14 @@ CAP_ARGS = {
     "webcam1": "v4l2src device=/dev/v4l/by-id/usb-046d_C270_HD_WEBCAM_2D4AA0A0-video-index0 ! video/x-raw,width=320,height=240,framerate=30/1 ! videoconvert ! appsink",
     "webcam2": "v4l2src device=/dev/v4l/by-id/usb-046d_C270_HD_WEBCAM_200901010001-video-index0 ! video/x-raw,width=320,height=240,framerate=30/1 ! videoconvert ! appsink",
     "ir":      "v4l2src device=/dev/v4l/by-id/usb-GroupGets_PureThermal__fw:v1.3.0__8003000b-5113-3238-3233-393800000000-video-index0 ! videoconvert ! appsink",
-    "arm":     "v4l2src device=/dev/v4l/by-id/usb-046d_C270_HD_WEBCAM_200901010001-video-index0 ! video/x-raw,width=320,height=240,framerate=30/1 ! videoconvert ! appsink",
+    "arm":     "nvarguscamerasrc sensor_id=0 ! 'video/x-raw(memory:NVMM),framerate=30/1,format=NV12,width=320,height=240' ! nvvidconv ! appsink",
 }
+
+# working terminal arm camera command:
+# gst-launch-1.0 nvarguscamerasrc sensor_id=0 ! 'video/x-raw(memory:NVMM),framerate=30/1,format=NV12' ! nvvidconv ! xvimagesink
+
+# old camera command in file on xavier
+# gst-launch-1.0 nvarguscamerasrc sensor_id=0 ! nvvidconv ! video/x-raw,format=NV12 ! videoscale ! video/x-raw,width=320,height=240 ! videorate ! video/x-raw,framerate=30/1 ! videoconvert ! appsink`
 
 CAMERA_SIZE = (320, 240)
 
