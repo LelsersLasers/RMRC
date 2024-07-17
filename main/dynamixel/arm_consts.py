11aae24fc9fe1453cb08649485b7202dda2e94e4
Millan Kumar <69917973+Lord-Lelsers@users.noreply.github.com>
2024-07-17 12:23:12 -0700

    Arm consts froom Jetson

--- main/dynamixel/arm_consts.py
+++ main/dynamixel/arm_consts.py
@@ -3,14 +3,14 @@ MAX_POSITION = 4096
 
 ARM_REST_POSES = {
     "j1": 1024,
-    "j2": 3640,
-    "j3": 1830,
+    "j2": 3500,
+    "j3": 1300,
     "j4": 4800,
 }
 
 ARM_JOINT_OFFSETS = { # ARM_JOINT_OFFSETS[joint] = reader_pos - jetson_pos
     "j1": 0,
-    "j2": 1500,
+    "j2": 900,
     "j3": -870,
     "j4": 0,
 }
